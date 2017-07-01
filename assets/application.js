a0f0b0a7efdf8120f833263a5c45222991f55b25
Dan Allison <daniel.j.allison@gmail.com>
2017-07-01 09:01:50 -0700

    Update assets

--- assets/application.js
+++ assets/application.js
@@ -37952,7 +37952,7 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
   // as the computed value while the user is typing
   var fnToString = _.constant('[Function]');
 
-  function ContextObject (item, includeScope) {
+  function ContextObject (item) {
     if (item) {
       var _this = this;
       _.each(itemMethods, function(method) {
@@ -37962,88 +37962,59 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
         _this[method].toString = fnToString;
       });
 
-      // var fns = item.$$item('functions');
-
-      // if (fns && fns.items.length) {
-      //   _.each(fns.items, function (fn) {
-      //     _this[fn.key] = function () {
-      //       return evalculist(fn.val)(_this);
-      //     };
-      //     _this[fn.key].toString = fnToString;
-      //   });
-      // }
-
-      if (includeScope) {
-        var parents = [],
-            nextParent = item.parent,
-            previousParent,
-            assignVar = function (sibling) {
-              var key = sibling.key.replace(/\s/g, '_');
-              if (sibling.hasVal) {
-                _this[key] = sibling.valueOf();
-                var loops = 0;
-                while (_this[key] && isItem(_this[key]) && _this[key].hasVal && ++loops < 100) {
-                  _this[key] = _this[key].valueOf();
-                }
-              } else {
-                _this[key] = sibling;
-              }
-            };
-        while (nextParent) {
-          parents.unshift(nextParent);
-          nextParent = nextParent.parent;
-        }
-        while (parents.length) {
-          previousParent = nextParent;
-          nextParent = parents.shift();
-          if (previousParent && nextParent) {
-            _.find(previousParent.items, function (sibling, i) {
-              assignVar(sibling);
-              return sibling === nextParent;
-            });
-          }
-        }
-        this.$index = 0;
-        var siblings = item.parent && item.parent.items;
-        if (siblings && siblings.length) {
-          _.find(siblings, function (sibling, i) {
-            if (sibling === item) {
-              _this.$index = i;
-              return true;
-            }
-            assignVar(sibling);
-          });
-        }
-      }
-
       this.$parent = item.parent;
       this.$index = item.parent ? item.parent.items.indexOf(item) : 0;
       this.$items = item.items;
-      this.$key = item.key;
       this.$name = item.key;
     }
   }
 
   var proto = ContextObject.prototype;
 
+  var valIfItem = function (item) { return isItem(item) ? item.valueOf() : item; };
+  var itemsIfItem = function (item) { return isItem(item) ? item.items : item; };
+  var itemsFirst = function (fn) {
+    return function () {
+      arguments[0] = itemsIfItem(arguments[0]);
+      return fn.apply(this, arguments);
+    };
+  };
+  var allItemsAndValues = function (fn) {
+    return function () {
+      var i = arguments.length;
+      while (--i >= 0) {
+        arguments[i] = itemsIfItem(arguments[i]);
+        if (_.isArray(arguments[i])) arguments[i] = _.map(arguments[i], proto.valueOf);
+      }
+      return fn.apply(this, arguments);
+    };
+  };
+
   var mathKeys = ['E','LN2','LN10','LOG2E','LOG10E','PI','SQRT1_2','SQRT2','abs','acos','acosh','asin','asinh','atan','atan2','atanh','cbrt','ceil','clz32','cos','cosh','exp','expm1','floor','fround','hypot','imul','log','log1p','log2','log10','max','min','pow','random','round','sign','sin','sinh','sqrt','tan','tanh','trunc'];
-
-  _.each(mathKeys, function(key) { proto[key] = Math[key]; });
-
   var ssKeys = ['median','mode','product','variance','sampleVariance','standardDeviation','sampleStandardDeviation','medianAbsoluteDeviation','interquartileRange','harmonicMean','geometricMean','rootMeanSquare','sampleSkewness','factorial'];
-
-  _.each(ssKeys, function(key) { proto[key] = ss[key]; });
-
   var lodashKeys = ['difference','intersection','reverse','union','uniq','xor',
                     'filter','find','findLast','map','reduce','zip','unzip',
                     'ceil','floor','max','min','round','clamp','inRange','range',
                     'camelCase','capitalize','kebabCase','lowerCase','join','split',
                     'parseInt','repeat','replace','snakeCase','split','startCase','toLower',
                     'toUpper','truncate','upperCase','words','times','identity'];
+  _.each(mathKeys, function(key) { proto[key] = Math[key]; });
+  _.each(ssKeys, function(key) { proto[key] = ss[key]; });
   _.each(lodashKeys, function (key) { proto[key] = _[key]; });
 
-  var valIfItem = function (item) { return isItem(item) ? item.valueOf() : item; };
-  var itemsIfItem = function (item) { return isItem(item) ? item.items : item; };
+  var iterators = ['average','mean','median','mode','standardDeviation','products',
+                  'reverse','unzip',
+                  'filter','find','findLast','map','reduce'];
+
+  _.each(iterators, function (methodName) {
+    proto[methodName] = itemsFirst(proto[methodName]);
+  });
+
+  var doubleIterators = ['difference','intersection','union','xor','zip'];
+
+  _.each(doubleIterators, function (methodName) {
+    proto[methodName] = allItemsAndValues(proto[methodName]);
+  });
 
   var conditionals = ['isArray','isNumber','isFunction','isString'];
 
@@ -38068,7 +38039,7 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
   proto.average = proto.mean = function (items, defaultValue) {
     return proto.sum(items, defaultValue) / items.length;
   };
-  proto.sum = function(items, defaultValue) {
+  proto.sum = itemsFirst(function(items, defaultValue) {
     if (defaultValue != null) {
       return items.reduce(function(sum, item) {
         var val = item && item.valueOf();
@@ -38080,26 +38051,16 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
       var val = item.valueOf();
       return sum + (_.isNumber(val) ? val : NaN);
     }, 0);
-  };
+  });
 
-  proto.quantile = function (sample, p) {
+  proto.quantile = itemsFirst(function (sample, p) {
     if (_.isNumber(p)) return ss.quantile(sample, p);
-  };
+  });
 
-  proto.interquartileRange = function (items) {
+  proto.interquartileRange = itemsFirst(function (items) {
     var values = _.map(items, valIfItem);
     return ss.interquartileRange(values);
-  };
-
-  // var multiOps =
-
-  proto.products = function (arr) {
-    return _.map(arr, function (pair) {
-      return pair.length && _.reduce(pair, function (a, b) { return +a * +b; }, 1);
-    });
-  };
-
-  // TODO sums, squares, cubes, sqrts,
+  });
 
   proto.binomialCoefficient = function binomial(n, k) {
     // if ((typeof n !== 'number') || (typeof k !== 'number')) return false;
@@ -38137,15 +38098,15 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
     return 360 * radians / (2 * Math.PI);
   };
 
-  proto.length = proto.count = _.property('length');
+  proto.length = proto.count = itemsFirst(_.property('length'));
   proto.name = proto.key = _.property('key');
   proto.valueOf = _.method('valueOf');
   proto.toString = _.method('toString');
-  proto.recursiveCount = proto.rcount = function (items) {
+  proto.recursiveCount = proto.rcount = itemsFirst(function (items) {
     return items.length + _.reduce(items, function (m, item) {
       return m + proto.recursiveCount(item.items);
     }, 0);
-  };
+  });
 
   proto.true = true;
   proto.false = false;
@@ -38157,26 +38118,24 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
   proto.itemsOf = _.property('items');
   proto.nameOf = _.property('key');
   proto.parentOf = _.property('parent');
-  proto.indexOf = function (array, item) {
-    array = itemsIfItem(array);
+  proto.indexOf = itemsFirst(function (array, item) {
     return array.indexOf(item);
-  };
-  proto.pluckItems = function (items, key) {
+  });
+  proto.pluckItems = itemsFirst(function (items, key) {
     var condition;
     if (_.isFunction(key)) {
       condition = key;
     } else {
       condition = { key: key };
     }
-    items = itemsIfItem(items);
     return items.reduce(function (pluckedItems, item) {
       var pluckedItem = _.find(item.items, condition);
       if (pluckedItem) pluckedItems.push(pluckedItem);
       return pluckedItems;
     }, []);
-  };
+  });
 
-  proto.flowMap = function () {
+  proto.flowMap = itemsFirst(function () {
     var fns = _.flatten(arguments);
     var fmap = _.rest(function (args) {
       return _.map(fns, function (fn) {
@@ -38185,9 +38144,9 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
     });
     fmap.toString = fnToString;
     return fmap;
-  };
+  });
 
-  proto.flow = function () {
+  proto.flow = itemsFirst(function () {
     var fns = _.flatten(arguments);
     var flow = function () {
       return _.reduce(fns, function (r, fn) {
@@ -38196,9 +38155,13 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
     };
     flow.toString = fnToString;
     return flow;
-  };
+  });
 
-  proto.compose = _.flip(proto.flow);
+  proto.compose = itemsFirst(function () {
+    var fns = _.flatten(arguments);
+    fns.reverse();
+    return proto.flow(fns);
+  });
 
   var curry2 = function (fn) {
     var fn0, fn1;
@@ -38276,30 +38239,16 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
     return proto.pluckItems(obj, key);
   };
 
-  proto.dotAccessor = function (items, key) {
-    items = itemsIfItem(items);
+  proto.dotAccessor = itemsFirst(function (items, key) {
     var item = _.findLast(items, function (item) {
       if (!item.key) item.valueOf();
       return keyToVarName(item.key) === key;
     });
     if (item && item.hasVal) return item.valueOf();
     return item;
-  };
-
-  var iterators = ['sum','count','average','mean','median','mode','standardDeviation','products',
-                  'reverse','unzip',
-                  'filter','find','findLast','map','reduce'];
-
-  _.each(iterators, function (methodName) {
-    var baseFn = proto[methodName];
-    proto[methodName] = function () {
-      if (isItem(arguments[0])) arguments[0] = arguments[0].items;
-      return baseFn.apply(this, arguments);
-    };
   });
 
-  proto.flatten = function (items) {
-    items = itemsIfItem(items);
+  proto.flatten = itemsFirst(function (items) {
     if (isItem(items[0])) {
       return _.reduce(items, function (flatItems, item) {
         flatItems.push(item);
@@ -38309,34 +38258,17 @@ calculist.register('createComputationContextObject', ['_','ss','evalculist','isI
     } else {
       return _.flatten(items);
     }
-  }
+  });
 
-  proto.uniq = proto.unique = function (items) {
-    items = itemsIfItem(items);
+  proto.uniq = proto.unique = itemsFirst(function (items) {
     items = _.map(items, proto.valueOf);
     return _.uniq(items);
-  };
+  });
 
-  proto.slice = function (items, start, end) {
-    items = itemsIfItem(items);
+  proto.slice = itemsFirst(function (items, start, end) {
     var result = _.slice(items, start, end);
     if (_.isString(items)) return result.join('');
     return result;
-  };
-
-  var doubleIterators = ['difference','intersection','union','xor','zip'];
-
-  _.each(doubleIterators, function (methodName) {
-    var baseFn = proto[methodName];
-    proto[methodName] = function () {
-      var args = arguments;
-      _.each(args, function (arg, i) {
-        if (isItem(arg)) arg = arg.items;
-        if (_.isArray(arg)) arg = _.map(arg, proto.valueOf);
-        args[i] = arg;
-      });
-      return baseFn.apply(this, args);
-    };
   });
 
   _.each(proto, function (fn, key) {
